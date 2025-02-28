96fd554d9427019c5fb052c397821f9d112c79eb
Abhishek Ingle <abhishekingle662@gmail.com>
2025-02-28 02:36:23 -0500

    googleanalytics file add now

---
+++ app/GoogleAnalytics.js
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect } from 'react';
+import Script from 'next/script';
+import { usePathname } from 'next/navigation';
+
+export default function GoogleAnalytics({ GA_MEASUREMENT_ID }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // Track page views
+    if (pathname && window.gtag) {
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: pathname,
+      });
+    }
+  }, [pathname, GA_MEASUREMENT_ID]);
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+}
